f95464f15310a7e4fcc23fb7e32ec2c0d4e038cc
BrandotheCluff <brandon.cluff2@gmail.com>
2022-07-28 16:45:30 -0600

    Sluggish

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-require('dotenv').config
+require('dotenv').config()
 
 const express = require('express')
 const app = express()
-
-
 const jwt = require('jsonwebtoken')
+
+app.use(express.json())
 
 const posts = [
     {
@@ -17,17 +17,34 @@ const posts = [
     }
 ]
 
-app.get('/posts', (req, res) => {
-    res.json(posts)
+
+
+app.get('/posts', authenticateToken, (req, res) => {
+    res.json(posts.filter(post => post.username === req.user.name))
 })
 
-app.get('/login', (req, res) => {
+app.post('/login', (req, res) => {
 
     const username = req.body.username
-    const user = { name: username}
+    const user = { name: username }
 
     const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
     res.json( {accessToken: accessToken} )
 })
 
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization']
+    const token = authheader && authHeader.split(' ')[1]
+   if(token ==null) return res.sendStatus(401)
+   
+   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+       if (err) return res.sendStatus(403)
+       req.user = user
+       next()
+   })
+   }
+
+
 app.listen(3000)
+
+console.log("All systems go")
